a8873aef2464bc51c34a848c1a13cefa8806e4da
ashish bisht <ashish.bisht@techugo.com>
2023-02-11 19:25:24 +0530

    added gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import swDev from './swDev';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+// swDev()
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base:'/demo-pwa/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'demo-pwa',
+        short_name: 'demopwa',
+        description: 'Website description(Could be same with index.html file)',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: 'demo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          }
+        ],
+      },
+    })
+  ],
 })
